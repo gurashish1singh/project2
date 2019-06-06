560f9615e3400bce097ba0bca1fbbf0108787af8
gurashishs <gurashish1singh@gmail.com>
2019-06-06 11:36:47 -0400

    Added a new line to model Modified message in views

--- myapp/views.py
+++ myapp/views.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
 
     context = {
-        'message' : 'Hello'
+        'message': 'Updating git'
     }
 
     return render(request, 'index.html', context)
